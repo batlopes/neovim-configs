e073730411ab2936ca11f9397bfb349e9ebf065a
Bruno Artur <batlopes@yahoo.com.br>
2022-01-31 22:59:28 -0300

    Customizing colorscheme configs

--- lua/user/colorscheme.lua
+++ lua/user/colorscheme.lua
@@ -1,8 +1,11 @@
-vim.cmd [[
-try
-  colorscheme darkplus
-catch /^Vim\%((\a\+)\)\=:E185/
-  colorscheme default
-  set background=dark
-endtry
-]]
+-- colorschemes installeds by packer:
+-- onedark, gruvbox, tokyonight, PaperColor
+
+-- default colorscheme
+local colorscheme = "onedark"
+
+local status_ok, _ = pcall(vim.cmd, "colorscheme " .. colorscheme)
+if not status_ok then
+  vim.notify("Colorscheme " .. colorscheme .. " not found!")
+  return
+end
