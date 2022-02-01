e5bf9277d31a6745992d83f2f34407a268efb09a
Bruno Artur <batlopes@yahoo.com.br>
2022-01-31 23:06:25 -0300

    Adding todo-comments plugin, and your respective config file

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -46,6 +46,9 @@ return packer.startup(function(use)
   use "nvim-lua/plenary.nvim" -- Useful lua functions used ny lots of plugins
   use "windwp/nvim-autopairs" -- Autopairs, integrates with both cmp and treesitter
   use "numToStr/Comment.nvim" -- Easily comment stuff
+  use {"folke/todo-comments.nvim",
+    requires = "nvim-lua/plenary.nvim"
+  }
   use "kyazdani42/nvim-web-devicons"
   use "kyazdani42/nvim-tree.lua"
   use "akinsho/bufferline.nvim"
